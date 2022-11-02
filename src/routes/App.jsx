8e18afc8a41a6ab5919ff2382dcb09d28008b6ff
DidierDev672 <ariasdev672@gmail.com>
2022-11-02 17:19:28 -0500

    Change in routing configuration, change of react-router-dom version

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout";
 
 import Home from "../containers/Home";
+import Know from "../containers/Know";
+import Contact from "../containers/Contact";
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />
-    }
-  ]);
-    return(
+  return(
+    <BrowserRouter>
       <Layout>
-          <RouterProvider  router={router}/>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/know" element={<Know />}/>
+          <Route path="/contact" element={<Contact />}/>
+        </Routes>
       </Layout>
-    );
+    </BrowserRouter>
+  );
 };
 
 export default App;
