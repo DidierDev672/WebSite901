acf2bad0c8f92503c8ecf1c2a60ec52b9e609ea2
DidierDev672 <ariasdev672@gmail.com>
2022-11-04 15:57:00 -0500

    Object where is the initiated data of fish farm product

---
+++ src/initialState.js
@@ -0,0 +1,46 @@
+export default {
+    product:[
+        {
+            "id": "1",
+            "catogory": "Sensor",
+            "name_product": "Sensor de oxigeno disuelto",
+            "price": 785500,
+            "description": "El sensor te permitira medir la cantidad de oxigeno disuelto presente en el estanque, los datos medidos son enviados por red wi-fi."
+        },
+        {
+            "id": "2",
+            "category": "Alimentador",
+            "name_product": "Alimentador semiautomatico",
+            "price": 483006,
+            "description": "Este equipo alimentar los peces con una racion de alimentos, previamente cargada mediante conexion wifi"
+        },
+        {
+            "id": "3",
+            "category": "Sensor",
+            "name_product": "Sensor de temperatura",
+            "price": 10000,
+            "description": "Este sensor mide la temperatura del agua en grado .C o .F con la cual podemos tomar decisiones a la hora de la alimentacion de los peces."
+        },
+        {
+            "id": "4",
+            "category": "Sensor",
+            "name_product": "Sensor de nivel de agua",
+            "price": 20900,
+            "description": "Este sensor permite calcular el volumen de agua medidos en m3, para poder decisiones y generar mensajes alarmas en casos."
+        },
+        {
+            "id": "5",
+            "category": "Sensor",
+            "name_product": "Sensor de potencial de hidrogeno",
+            "price": 12000,
+            "description": "El sensor de PH mide valores de pontencial de hidrogeno en una escala de 0 a 14, con estos datos se pueden tomar decisiones."
+        },
+        {
+            "id": "6",
+            "category": "Biometria",
+            "name_product": "Equipo inteligente de biometria",
+            "price": 229890,
+            "description": "Este equipo permite obtener el peso promedio del pez en el estanque, con cual se puede obtener la biomasa promedio de esa produccion."
+        }
+    ]
+};
