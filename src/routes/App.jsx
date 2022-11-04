32ee73ef283efabf0b9094375f8023e4200e9ad0
DidierDev672 <ariasdev672@gmail.com>
2022-11-04 14:43:55 -0500

    Addition of a new path to the login and register user website

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,6 +5,8 @@ import Layout from "../components/Layout";
 import Home from "../containers/Home";
 import Know from "../containers/Know";
 import Contact from "../containers/Contact";
+import SignIn from "../containers/SignIn";
+import SignUp from "../containers/SignUp";
 
 const App = () => {
   return(
@@ -14,6 +16,8 @@ const App = () => {
           <Route path="/" element={<Home />}/>
           <Route path="/know" element={<Know />}/>
           <Route path="/contact" element={<Contact />}/>
+          <Route path="/sign-in" element={<SignIn />}/>
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </Layout>
     </BrowserRouter>
