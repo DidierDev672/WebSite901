fbfd6c4eb3ac97690e0af3fc146ae1da2cd8d1d4
DidierDev672 <ariasdev672@gmail.com>
2022-11-07 18:50:02 -0500

    Incorporation of the states in all the website

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -7,20 +7,29 @@ import Know from "../containers/Know";
 import Contact from "../containers/Contact";
 import SignIn from "../containers/SignIn";
 import SignUp from "../containers/SignUp";
+import Products from "../containers/Products";
+import ShoppingCart from "../containers/ShoppingCart";
+import AppContext from "../context/AppContext.js";
+import useInitialState from "../hooks/useInitialState.js";
 
 const App = () => {
+  const initialState = useInitialState();
   return(
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/know" element={<Know />}/>
-          <Route path="/contact" element={<Contact />}/>
-          <Route path="/sign-in" element={<SignIn />}/>
-          <Route path="/sign-up" element={<SignUp />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/know" element={<Know />}/>
+            <Route path="/contact" element={<Contact />}/>
+            <Route path="/products" element={<Products />}/>
+            <Route path="/shopping-bag" element={<ShoppingCart />}/>
+            <Route path="/sign-in" element={<SignIn />}/>
+            <Route path="/sign-up" element={<SignUp />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
