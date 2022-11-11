65b3a5cdde16d7124c6cf61f680c1563c4ba7781
DidierDev672 <ariasdev672@gmail.com>
2022-11-11 00:12:46 -0500

    Now search in the categories

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,13 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useState,useRef} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import ItemCategory from "./ItemCategory.jsx";
 import AppContext from "../context/AppContext.js";
 import "../styles/components/category.scss";
 
+
 const Category = () => {
     const { state } = useContext(AppContext);
     const { category } = state;
+    const [ search, setSearch ] = useState(category);
+    const searchRef = useRef(null);
+
+    const handleSearch = () => {
+        if(searchRef.current.value !== ""){
+            const Refsearch = searchRef.current.value;
+            setSearch(category.filter((item) => {
+                return item.title.toLowerCase().includes(Refsearch.toLowerCase())
+            }))
+        }
+    };
+
 
     return(
         <div className="container">
@@ -15,11 +28,11 @@ const Category = () => {
             <h5>Categoria de productos</h5>
         </div>
         <div className="content-search-category">
-            <input type="text" className="field-text" placeholder="Ingresa una categoria"/>
-            <button type="button" className="btn-search"> <FontAwesomeIcon icon={faMagnifyingGlass}/> </button>
+            <input type="text" className="field-text" placeholder="Ingresa una categoria" ref={searchRef}/>
+            <button type="button" className="btn-search" onClick={handleSearch}> <FontAwesomeIcon icon={faMagnifyingGlass}/> </button>
         </div>
             <div className="flex-category py-3">
-                {category.map((categorys) => (
+                {search.map((categorys) => (
                     <ItemCategory key={categorys.id} categorys={categorys}/>
                 ))}
             </div>
