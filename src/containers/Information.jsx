084b69f12d02cae6681b98abe9bf12384f93b990
DidierDev672 <ariasdev672@gmail.com>
2022-11-11 23:43:11 -0500

    Parent Component, Where we will obtain the user's data

---
+++ src/containers/Information.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DataBasic from "../components/Databasic";
+
+const Information = () => {
+    return(
+        <div className="container py-3">
+            <DataBasic />
+        </div>
+    );
+};
+
+export default Information;
