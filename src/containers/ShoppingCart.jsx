663e53e45cb651d17492f4b813f43d8d3db110a9
DidierDev672 <ariasdev672@gmail.com>
2022-11-11 23:56:02 -0500

    Correction in the trolley arrangements

--- src/containers/ShoppingCart.jsx
+++ src/containers/ShoppingCart.jsx
@@ -7,7 +7,7 @@ import "../styles/components/shoppingcart.scss";
 
 const ShoppingCart = () => {
     const { state, removeFromCart } = useContext(AppContext);
-    const { cart } = state;
+    const { trolley } = state;
 
     const handleRemove = product => () => {
         removeFromCart(product);
@@ -15,7 +15,7 @@ const ShoppingCart = () => {
 
     const handleSumTotal = () => {
         const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
-        const sum = cart.reduce(reducer, 0);
+        const sum = trolley.reduce(reducer, 0);
         return sum;
     };
 
@@ -24,33 +24,31 @@ const ShoppingCart = () => {
             <div className="shopping-bag">
                 <div className="card-shopping-bag">
                     <br />
-                    { cart.length > 0 ? <h3>Lista de pedidos</h3> : <h3>Sin pedidos...</h3> }
+                    { trolley.length > 0 ? <h3>Lista de pedidos</h3> : <h3>Sin pedidos...</h3> }
                     <table className="table">
                         <thead>
                             <tr>
-                                <th scope="col" className="font-table-header">#</th>
                                 <th scope="col" className="font-table-header">Nombre producto</th>
                                 <th scope="col" className="font-table-header">Precio</th>
                                 <th scope="col" className="font-table-header">Eliminar</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {cart.map((item) => (
-                                <tr key={item.id}>
-                                    <th scope="row" className="font-table-body">{item.id}</th>
-                                    <th className="font-table-body">{item.name_product}</th>
-                                    <th className="font-table-body">${item.price}</th>
-                                    <th><button type="button" className="btn-delete-shopping" onClick={handleRemove(item)}><FontAwesomeIcon  icon={faTrash}/></button></th>
+                            {trolley.map((product) => (
+                                <tr key={product.id}>
+                                    <th className="font-table-body">{product.name_product}</th>
+                                    <th className="font-table-body">${product.price}</th>
+                                    <th><button type="button" className="btn-delete-shopping" onClick={handleRemove(product)}><FontAwesomeIcon  icon={faTrash}/></button></th>
                                 </tr>
                             ))}
                         </tbody>
                     </table>
                 </div>
             </div>
-            {cart.length > 0 && (
+            {trolley.length > 0 && (
                 <div className="shopping-sidebar">
                     <h3>{`Precio Total $ ${handleSumTotal()}`}</h3>
-                    <Link to={`/shopping/information`}>
+                    <Link to={`/shopping-bag/information`}>
                         <button type="button" className="btn-continue-order">Continuar pedido</button>
                     </Link>
                 </div>
