9610b8c237cd6f0abcca69bda96bcb695823ff51
DidierDev672 <ariasdev672@gmail.com>
2022-11-11 23:46:52 -0500

    Individual purchase form

---
+++ src/containers/Purchase.jsx
@@ -0,0 +1,39 @@
+import React, { useContext,useState,useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import DataBasic from "../components/Databasic";
+import AppContext  from "../context/AppContext.js";
+import "../styles/components/purchase.scss";
+
+const Purchase = () => {
+    const { id } = useParams();
+    const { state } = useContext(AppContext);
+    const [ result, setResult ] = useState([]);
+    const { products } = state;
+
+    useEffect(() => {
+        setResult( products.filter((item) => item.id == id))
+    },[]);
+    return(
+        <div className="container py-3">
+            <div className="mb-3">
+                <div className="row g-0">
+                    <div className="col-md-6">
+                            {result.map((item) => (
+                                <div className="card-purchase" key={item.id}>
+                                    <img src={item.coverURL} className="img-thumbnail" alt={item.name_product}/>
+                                    <h4>{item.name_product}</h4>
+                                    <span>$ {item.price}</span>
+                                    <p>{item.description}</p>
+                                </div>
+                            ))}
+                    </div>
+                    <div className="col-md-6">
+                        <DataBasic />
+                    </div>
+                </div>
+            </div>
+    </div>
+    );
+};
+
+export default Purchase;
