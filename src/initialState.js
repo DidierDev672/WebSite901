0e93fccb9a082f31649a48a30ea44522bbc6ec9f
DidierDev672 <ariasdev672@gmail.com>
2022-11-23 01:33:20 -0500

    New data of our values, website for about the project

--- src/initialState.js
+++ src/initialState.js
@@ -4,6 +4,38 @@ export default {
     trolley: [],
     buyer: [],
     orders: [],
+    values: [
+        {
+            id: "1",
+            values: "Creatividad",
+            description: "Intentamos aportar imaginación, ideas originales  y emoción a todo lo que hacemos."
+        },
+        {
+            id: "2",
+            values: "Iniciativa",
+            description: "Actuamos con la curiosidad y el valor necesarios para experimentar, innovar y liderar."
+        },
+        {
+            id: "3",
+            values: "Pasión",
+            description: "Rendimos más cuando hacemos lo que nos gusta y nos divertimos haciéndolo."
+        },
+        {
+            id: "4",
+            values:"Determinación",
+            description: "Aportamos decisión, ímpetu y convicción a nuestras acciones. Disfrutamos con el viaje y nos motiva alcanzar la excelencia."
+        },
+        {
+            id: "5",
+            values: "Aprendizaje",
+            description: "Escuchamos, actuamos con humildad, no nos cerramos a nuevas formas de pensar y siempre tenemos en mente la inclusión. Nos planteamos el reto de crecer y cambiar como empresa."
+        },
+        {
+            id: "6",
+            values: "Trabajo en equipo",
+            description: "Nos comprometemos con los demás y con la responsabilidad e integridad necesarias para ser un equipo diverso de éxito."
+        }
+    ],
     category:[
         {
             id: "1",
