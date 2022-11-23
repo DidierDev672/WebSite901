67d8327ae48b5f1e9354e127f437ad8cd57a1871
DidierDev672 <ariasdev672@gmail.com>
2022-11-23 01:25:50 -0500

    Firebase configuration 🔥

---
+++ src/api/settings.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyA5CR1QaR74ZIVaV30utUvqQe38QKPQplQ",
+    authDomain: "website-96258.firebaseapp.com",
+    projectId: "website-96258",
+    storageBucket: "website-96258.appspot.com",
+    messagingSenderId: "1066305193021",
+    appId: "1:1066305193021:web:1df5b2c84b18f8dfdd9654",
+    measurementId: "G-6JNXT2909D"
+}
