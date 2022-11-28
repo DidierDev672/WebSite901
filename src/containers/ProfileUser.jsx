5c72bf7e5cc09bc6e278102eb344319822e5be38
DidierDev672 <ariasdev672@gmail.com>
2022-11-27 22:48:41 -0500

    Incorporation of auth ot the firebase services on the web, playa pez

--- src/containers/ProfileUser.jsx
+++ src/containers/ProfileUser.jsx
@@ -1,142 +1,52 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import {  onAuthStateChanged  } from "firebase/auth";
+import { auth } from "../api/firebase.js";
+import Itemprofile from "../components/handling/Itemprofile";
 import AppContext from "../context/AppContext.js";
 import "../styles/components/profile.scss";
 
+const ProfileFirestore = () => {
+    const { state } = useContext(AppContext);
+    const { profile } = state;
+    return(
+        <>
+            {profile.map((item) => (
+                <Itemprofile key={item.id} profile={item}/>
+            ))}
+        </>
+    );
+};
+
+const ProfileAuth = ({ user }) => {
+    return(
+        <>
+            <Itemprofile key={user.uid} profile={user}/>
+        </>
+    );
+};
+
 const ProfileUser = () => {
     const { state } = useContext(AppContext);
-    const { user } = state;
-    const [ profile, setProfile ] = useState(...user);
+    const {  profile } = state;
+    const [ user, setUser ] = useState([]);
 
-    function handleNamefullChange(e){
-        setProfile({
-            ...profile,
-            namefull: e.target.value
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(user){
+                setUser({
+                    uid: user.uid,
+                    email: user.email,
+                    namefull: user.displayName
+                });
+            }
         })
-    }
-
-    function handlePhoneChange(e){
-        setProfile({
-            ...profile,
-            phone: e.target.value
-        })
-    }
-
-    function handleCountryChange(e){
-        setProfile({
-            ...profile,
-            country: e.target.value
-        })
-    }
-
-    function handleCityChange(e){
-        setProfile({
-            ...profile,
-            city: e.target.value
-        })
-    }
-
-    function handleAddressChange(e){
-        setProfile({
-            ...profile,
-            address: e.target.value
-        })
-    }
-
-    function handleEmailChange(e){
-        setProfile({
-            ...profile,
-            email: e.target.value
-        })
-    }
+    }, []);
 
     return(
         <div className="container py-5">
+            <div className="py-3"></div>
             <div className="card-profile">
-                <h4>Perfil  { profile.namefull }</h4>
-                <form>
-                    <div className="flex-profile">
-                    <div className="item-profile">
-            <label>Nombre completo</label>
-            <input
-            type="text"
-            name="namefull"
-            className="field-text"
-            value={profile.namefull}
-            onChange={handleNamefullChange}
-            placeholder="Juan perez"
-            />
-        </div>
-        <div className="item-profile">
-            <label>Telefono</label>
-            <input
-            type="tel"
-            name="phone"
-            className="field-text"
-            value={profile.phone}
-            onChange={handlePhoneChange}
-            placeholder="000-000-0000"
-            />
-        </div>
-        <div className="item-profile">
-            <label>Pais</label>
-            <input
-            type="text"
-            name="country"
-            className="field-text"
-            value={profile.country}
-            onChange={handleCountryChange}
-            placeholder="Colombia"
-            />
-        </div>
-        <div className="item-profile">
-            <label>Ciudad</label>
-            <input
-            type="text"
-            name="city"
-            className="field-text"
-            value={profile.city}
-            onChange={handleCityChange}
-            placeholder="Cauciasia-Antioquia"
-            />
-        </div>
-        <div className="item-profile">
-            <label>Direccion</label>
-            <input
-            type="text"
-            name="address"
-            className="field-text"
-            value={profile.address}
-            onChange={handleAddressChange}
-            placeholder="Calle # N A # 40"
-            />
-        </div>
-        <div className="item-profile">
-            <label>Correo electronico </label>
-            <input
-            type="email"
-            name="email"
-            className="field-text"
-            placeholder="example@example.com"
-            value={profile.email}
-            onChange={handleEmailChange}
-            />
-        </div>
-        <div className="item-profile">
-            <label>Password</label>
-            <input
-            type="password"
-            name="pwd"
-            className="field-text"
-            placeholder="**********"
-            />
-        </div>
-        <div className="item-profile py-3">
-            <button type="button" className="btn-profile">
-            Actualizar
-            </button>
-        </div>
-                    </div>
-                </form>
+                { profile.length > 0 ? <ProfileAuth user={user} /> : <ProfileFirestore /> }
             </div>
         </div>
     );
