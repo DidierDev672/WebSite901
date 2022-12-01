76dec95755fde1590e9d693abae34136b5072a3f
DidierDev672 <ariasdev672@gmail.com>
2022-12-01 22:08:54 +0100

    Change of designs in the add direct purchase buttons

--- src/components/SubProduct.jsx
+++ src/components/SubProduct.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
 import {  FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import AppContext from "../context/AppContext";
 import "../styles/components/products.scss";
 
 const SubProduct = ({ product,handleAddToCart }) => {
+    const { addToCart, addNewOrder  } = useContext(AppContext);
+    const handleAddOrder = product => () => {
+        addNewOrder(product);
+        addToCart(product);
+    };
     return(
         <div className="Products-item">
             <div className="Product-item-ifno">
-                <img className="img-thumbnail" src={product.coverURL} alt={product.name_product} width="512px" height="512px"  />
+                <img className="img-thumbnail" src={product.coverURL} alt={product.name_product} width="420px" height="420px"  />
                 <h2 className="title-product-item">
                     {product.name_product}
                     <br />
@@ -25,7 +31,7 @@ const SubProduct = ({ product,handleAddToCart }) => {
             <div className="flex-product-item">
                 <button type="button" className="btn-add-product" onClick={handleAddToCart(product)}> <FontAwesomeIcon icon={faPlus}/> </button>
                 {/* <button type="button" className="btn-buy-product"><FontAwesomeIcon icon={faCartShopping}/>  </button> */}
-                <Link className="btn-buy-product" to={`/purchase/${product.id}`}><FontAwesomeIcon icon={faCartShopping}/></Link>
+                <Link className="btn-buy-product" to={`/purchase/${product.id}`} onClick={handleAddOrder(product)}><FontAwesomeIcon icon={faCartShopping}/></Link>
             </div>
         </div>
     );
