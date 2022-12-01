d6b7f48088dc2e96fe966d5cf91f7c9044ccca3c
DidierDev672 <ariasdev672@gmail.com>
2022-12-01 06:53:43 +0100

    Soluction banner where we indicate a short speech about the project

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import VideoComponent from "../components/VideoComponent";
 import "../styles/components/home.scss";
+
+const UrlImg = new URL("../assets/J7VL3TQJXVA7JJCO2B7XIDRPFQ.jpg",import.meta.url).href;
 
 const Home = () => {
     return(
         <div className="container-fluid">
         <div className="py-5"></div>
             <VideoComponent />
+            <div className="content-home-soluction">
+                <div className="grid-home">
+                    <div className="item-home">
+                        <img src={UrlImg}/>
+                    </div>
+                    <div className="item-home">
+                        <div className="content-paragraph-home">
+                            <p>
+                                Una plataforma inteligente y sostenible para la producción terrestre de peces naturales, saludables y nutritivos.
+                            </p>
+                            <div className="item-home">
+                            <Link className="link-soluction-home" to={`solution`}>Solucion</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
