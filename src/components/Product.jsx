a1e4bd22e46613dcb1c47df7eb59a41af5d3de9a
DidierDev672 <ariasdev672@gmail.com>
2022-12-06 08:19:50 +0100

    Get all products, from firestore and validation of search by category

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,49 +1,34 @@
-import React, { useContext,useState, useRef, useEffect } from "react";
-import { useParams  } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import API from "../api";
 import SubProduct from "../components/SubProduct";
-import AppContext from "../context/AppContext.js";
 import "../styles/components/products.scss";
 
 const Product = () => {
+    const [ products, setProduct ] = useState([]);
     const { category } = useParams();
-    const { state, addToCart } = useContext(AppContext);
-    const { products } = state;
-    const [ product, setProduct ] = useState(...products);
-    const searchRef = useRef(null);
-
     useEffect(() => {
-        setProduct(product.filter((item) => item.category === category));
+        API.queryAllProducts()
+        .then((result) => {
+            const query = result.filter((item) => item.category === category);
+            if(query.length > 0){
+                setProduct(query);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        })
     });
-
-    const handleSearch = () => {
-        if(searchRef.current.value !== ""){
-            const Refsearch = searchRef.current.value;
-            setReSearch(products.filter((item) => {
-                return item.name_product.toLowerCase().includes(Refsearch.toLowerCase());
-            }))
-        }
-    };
-
-    const handleAddToCart = product => () => {
-        addToCart(product);
-    };
-
     return(
-        <div className="container py-3">
-        <div className="py-5"></div>
-            <div className="header-product">
-                <h4>Lista de productos {category}</h4>
+        <div className="container">
+            <div className="py-5"></div>
+            <div className="content-product">
+                <h4>Lista de productos {category}.</h4>
             </div>
-            <div className="field-search">
-                <input type="text" className="field-text" placeholder="Ingresa el nombre del producto" ref={searchRef}/>
-                <button type="button" className="btn-search-product" onClick={handleSearch}><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
-            </div>
-            <div className="Products-items">
-                { product.map((product) => (
-                    <SubProduct key={product._id} product={product} handleAddToCart={handleAddToCart}/>
-                )) }
+            <div className="content-list-product">
+                {products.map((item) => (
+                    <SubProduct key={item.id} product={item}/>
+                ))}
             </div>
         </div>
     );
