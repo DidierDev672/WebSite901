2978368da59ecfaf5904b493b7a8b67bdeab72e2
DidierDev672 <ariasdev672@gmail.com>
2022-12-06 12:48:17 -0500

    Blog Post

--- src/initialState.js
+++ src/initialState.js
@@ -1,90 +1,165 @@
 export default {
-    user: [],
-    cart: [],
-    profile: [],
-    trolley: [],
-    buyer: [],
-    orders: [],
-    contact: [
-        {
-            id: "1",
-            phone: "+57 3205737554",
-            email: "joseangel83@gmail.com",
-            location: "Apayel-Corboba"
-        }
-    ],
-    bracket:[
-        {
-            id: "1",
-            title: "Soporte al cliente",
-            description: "Nuestro equipo integral incluye apoyo continuo, incluyendo la cría de peces."
-        },
-        {
-            id: "2",
-            title: "Capacitación",
-            description: "Entrenaremos a tu equipo, para la instalación de operaciones cómo a lo largo de su ciclo de vida."
-        },
-        {
-            id: "3",
-            title: "Actualizaciones",
-            description: "A medida que tus necesidades operacionales cambian y las nuevas innovaciones tecnológicas están disponibles."
-        },
-        {
-            id: "4",
-            title: "Macro Data",
-            description: "Nuestra vasta base de conocimientos, que cubre todos los aspectos de la producción pesquera, está a tu disposición."
-        }
-    ],
-    values: [
-        {
-            id: "1",
-            values: "Creatividad",
-            description: "Intentamos aportar imaginación, ideas originales  y emoción a todo lo que hacemos."
-        },
-        {
-            id: "2",
-            values: "Iniciativa",
-            description: "Actuamos con la curiosidad y el valor necesarios para experimentar, innovar y liderar."
-        },
-        {
-            id: "3",
-            values: "Pasión",
-            description: "Rendimos más cuando hacemos lo que nos gusta y nos divertimos haciéndolo."
-        },
-        {
-            id: "4",
-            values:"Determinación",
-            description: "Aportamos decisión, ímpetu y convicción a nuestras acciones. Disfrutamos con el viaje y nos motiva alcanzar la excelencia."
-        },
-        {
-            id: "5",
-            values: "Aprendizaje",
-            description: "Escuchamos, actuamos con humildad, no nos cerramos a nuevas formas de pensar y siempre tenemos en mente la inclusión. Nos planteamos el reto de crecer y cambiar como empresa."
-        },
-        {
-            id: "6",
-            values: "Trabajo en equipo",
-            description: "Nos comprometemos con los demás y con la responsabilidad e integridad necesarias para ser un equipo diverso de éxito."
-        }
-    ],
-    category:[
-        {
-            id: "1",
-            title: "Medidor"
-        },
-        {
-            id: "2",
-            title: "Sensor",
-        },
-        {
-            id: "3",
-            title: "Alimentador"
-        },
-        {
-            id: "4",
-            title: "Biometria"
-        }
-    ],
-    products:[],
-    people:[]
+  user: [],
+  cart: [],
+  profile: [],
+  trolley: [],
+  buyer: [],
+  orders: [],
+  contact: [
+    {
+      id: '1',
+      phone: '+57 3205737554',
+      email: 'joseangel83@gmail.com',
+      location: 'Apayel-Corboba',
+    },
+  ],
+  bracket: [
+    {
+      id: '1',
+      title: 'Soporte al cliente',
+      description:
+        'Nuestro equipo integral incluye apoyo continuo, incluyendo la cría de peces.',
+    },
+    {
+      id: '2',
+      title: 'Capacitación',
+      description:
+        'Entrenaremos a tu equipo, para la instalación de operaciones cómo a lo largo de su ciclo de vida.',
+    },
+    {
+      id: '3',
+      title: 'Actualizaciones',
+      description:
+        'A medida que tus necesidades operacionales cambian y las nuevas innovaciones tecnológicas están disponibles.',
+    },
+    {
+      id: '4',
+      title: 'Macro Data',
+      description:
+        'Nuestra vasta base de conocimientos, que cubre todos los aspectos de la producción pesquera, está a tu disposición.',
+    },
+  ],
+  values: [
+    {
+      id: '1',
+      values: 'Creatividad',
+      description:
+        'Intentamos aportar imaginación, ideas originales  y emoción a todo lo que hacemos.',
+    },
+    {
+      id: '2',
+      values: 'Iniciativa',
+      description:
+        'Actuamos con la curiosidad y el valor necesarios para experimentar, innovar y liderar.',
+    },
+    {
+      id: '3',
+      values: 'Pasión',
+      description:
+        'Rendimos más cuando hacemos lo que nos gusta y nos divertimos haciéndolo.',
+    },
+    {
+      id: '4',
+      values: 'Determinación',
+      description:
+        'Aportamos decisión, ímpetu y convicción a nuestras acciones. Disfrutamos con el viaje y nos motiva alcanzar la excelencia.',
+    },
+    {
+      id: '5',
+      values: 'Aprendizaje',
+      description:
+        'Escuchamos, actuamos con humildad, no nos cerramos a nuevas formas de pensar y siempre tenemos en mente la inclusión. Nos planteamos el reto de crecer y cambiar como empresa.',
+    },
+    {
+      id: '6',
+      values: 'Trabajo en equipo',
+      description:
+        'Nos comprometemos con los demás y con la responsabilidad e integridad necesarias para ser un equipo diverso de éxito.',
+    },
+  ],
+  category: [
+    {
+      id: '1',
+      title: 'Medidor',
+    },
+    {
+      id: '2',
+      title: 'Sensor',
+    },
+    {
+      id: '3',
+      title: 'Alimentador',
+    },
+    {
+      id: '4',
+      title: 'Biometria',
+    },
+  ],
+  people: [],
+  authors: [
+    {
+      id: 1,
+      name_author: 'Didier Arias',
+      section: ['Technology', 'Science & Innovation', 'Others'],
+      twitter: 'https://twitter.com/Aizen0073',
+      facebook: 'https://www.facebook.com/Aizen.Arias.007',
+      instagram: 'https://www.instagram.com/aizenarias/',
+    },
+  ],
+
+  blogHeader:[
+    {
+      idPost: "pot-1",
+      titlePost: "¿Qué es una estructura narrativa?",
+      description: "Desde el punto de vista técnico, una estructura narrativa es el esqueleto de nuestra historia. Nos dice que elemento usar, cuando y donde para poder generar la mayor empatía posible en las personas que escuchen, lean o vean nuestra historia."
+    }
+  ],
+
+  blogDetalle: [
+    {
+      idPost: 'post-1',
+      idAuthor: 1,
+      titleOne: '¿Qué es una estructura narrativa?',
+      paragraphOne: [
+        'Desde el punto de vista técnico, una estructura narrativa es el esqueleto de nuestra historia. Nos dice que elemento usar, cuando y donde para poder generar la mayor empatía posible en las personas que escuchen, lean o vean nuestra historia.',
+        'Lo anterior se debe a que si nuestros espectadores sienten empatía hacia que se les cuenta, entonces sus reacciones serán genuinas y el mensajes que queramos transmitir quedará claro.',
+      ],
+      titleTwo: 'Como funciona las estructuras narrativas',
+      paragraphTwo: [
+        'Es verdad que el seguir una estructura narrativa tendemos a vernos encasillados en contar las cosas de forma aparentemente específica y es posible que esto haga que nuestra historia resulte similar a otra, este es el motivo por el cual muchos escritores se encuentran en contar de estos métodos.',
+        'Para empezar, las estructuras narrativas fueron diseñadas por expertos en la industria, cada pieza que las estructuras la sugieren a tus historia tiene un motivo y es algo que ha sido probado miles de veces con resultados positivos. A su vez, ayuda a brindarle coherencia a tu historia, conectar ideas y generar un cambio en tu protagonista.',
+        'Inclusive, ayudan a acabar con el famoso y temido bloqueo del escritor porque le brindan claridad a tu trama y al recorrido que tus personajes deben hacer.',
+      ],
+      titleThree: 'Cómo lograr una estructura narrativa',
+      paragraphThree: [
+        'Un ejemplo de lo que proponen las estructuras narrativas es que cada historia debe basarse en un planteamiento, una confrontación y una resolución. Esto parece algo básico, y lo es ya que inclusive esos son los fundamentos de la estructura de los 3 actos que ha sido usada desde los tiempos de Aristoteles hasta hoy en día ha sido utilizada en todos tipos de narrativa, incluido el teatro.',
+        'A Pesar de los básicos que sean y sus antecedentes, en sorprendente el número de historias que se olvidan de alguno de estos factores esenciales que sirven como base.Y las repercusiones de estos son evidentes!',
+      ],
+      titleFour: 'Tipos de estructuras que existen',
+      paragraphFour: [
+        'Hay varias estructuras que se encargan de enfatizar cada uno de estos elementos, presentando una serie de pasos intermedios para detallar estos tres momentos básicos y proponiendo un orden en el que estos pasos deben suceder. Algunos ejemplos de esto son:',
+      ],
+      listParagrapFour: [
+        'El camino del Héroe',
+        'Save the cat',
+        'La estructura de los 7 pilares.',
+      ],
+      subParagraohFour:
+        'Esos momentos progresivos e indispensables que estas estructuras proponen y que deben suceder son conocidos como “beats” y son justamente los que tienen una connotación mayor a una escena y hacen que la historia avance. El mejor modo para saber si un beat funciona o no es eliminándolo. Si el beat funciona, la historia no debería funcionar sin él.',
+      titleFive: 'Cuenta una buena historia',
+      paragrapFive: [
+        'Las historias no se basan solo en su trama. Una buena historia debe tener personajes claros, empáticos y que generen una reacción en los espectadores o de lo contrario el camino que recorran no nos será atractivo.',
+        'Se necesita que tu audiencia quiera que tu protagonista logre su meta y presente un cambio. Es por eso que las estructuras proponen a un tipo de protagonista que es el que se lleva usando desde que tenemos memoria.',
+        'Un ejemplo de esto se encuentra en el tipo de estructura llamado Save the cat. Ese método fue creado por Blake Snyder debido a que él se dio cuenta de que las películas no se tratan solo de una historia, sino de la forma en la que el público reacciona antes ellas.',
+        'Según se estudió sobre esto, lo que propone en cuanto a protagonistas es que este debe ser ya sea mejor, el bueno, el gracioso, el que sufre   o  el que está en desventaja. Si tu protagonista cumple con mínimo una de estas características, las posibilidades de que tu audiencia lo apoye y quiera conocerle aumentan considerablemente.',
+        'Como conclusión, si es una realidad que una historia te propone el recorrido que un protagonista debe hacer y esto puede parecer limitante, pero la realidad es que es lo opuesto. Las estructuras están para ayudarnos y hacer que nuestras historias se vuelvan más claras y nuestros personajes más empáticos.',
+        'Hay que tomar en cuenta que si se desea producir algo comercial, lo más probable es que para poder vender una historia sea un requisito el seguir alguna de estas estructuras. Esto no significa que nuestra habilidad creativa se pierda o que nuestra historia deba volverse necesariamente predecible.',
+        'Cuando uno entiende la esencia de las estructuras es completamente posible y recomendado el darles nuestra propia voz, estilo y asegurarnos de que sean utilizadas para contar lo que realmente queremos contar.',
+        'Necesitamos verlas más como un esqueleto que como un mapa. No son algo que debamos simplemente seguir paso a paso sin comprenderlas, son los que hace que nuestra historia funcione y se mueve, pero jamás tiene la intención de hacer que pierda su ausencia.',
+      ],
+      subParagraohFive: [
+        'Como conclusión, aunque las estructuras son las bases de nuestra historia y las debemos utilizar para brindarles. coherencia, no podemos permitir que por seguirles la ausencia de lo queremos contar desaparezca, por eso es que es necesario ciçonocerlas mucho más allá de la superficie, tenemos que entender por qué cada existe cada beat y cual es su finalidad, solo así podremos adaptarlos a lo que realmente queremos contar.',
+      ],
+    },
+  ],
 };
