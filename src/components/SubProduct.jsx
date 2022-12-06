5ac3b1a0d61f51b7da2b10ac360f73535d17c0c1
DidierDev672 <ariasdev672@gmail.com>
2022-12-06 08:23:28 +0100

    Change in the design of the add and delete buttons of the product bag

--- src/components/SubProduct.jsx
+++ src/components/SubProduct.jsx
@@ -1,39 +1,29 @@
-import React, { useContext } from 'react';
-import { Link } from "react-router-dom";
-import {  FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import AppContext from "../context/AppContext";
+import React from 'react';
+import { useSelector,useDispatch } from "react-redux";
+import { addProductToCart, removeProductFromCart } from "../reducers/cart/cartSlice";
 import "../styles/components/products.scss";
 
-const SubProduct = ({ product,handleAddToCart }) => {
-    const { addToCart, addNewOrder  } = useContext(AppContext);
-    const handleAddOrder = product => () => {
-        addNewOrder(product);
-        addToCart(product);
+const SubProduct = ({ product }) => {
+    const dispatch = useDispatch();
+    const { productsList } = useSelector(state => state.cart);
+    const handleAddOrRemoveProduct = (product) => {
+        if(productsList.find(pdt => pdt.id === product.id)){
+            dispatch(removeProductFromCart(product.id));
+        }else{
+            dispatch(addProductToCart(product));
+        }
     };
     return(
-        <div className="Products-item">
-            <div className="Product-item-ifno">
-                <img className="img-thumbnail" src={product.coverURL} alt={product.name_product} width="420px" height="420px"  />
-                <h2 className="title-product-item">
-                    {product.name_product}
-                    <br />
-                    <span className="sub-price-product-item">
-                        $
-                        {''}
-                        {product.price}
-                    </span>
-                </h2>
-                <br />
-                <p className="sub-title-product-item">{product.description}</p>
-            </div>
-            <div className="flex-product-item">
-                <button type="button" className="btn-add-product" onClick={handleAddToCart(product)}> <FontAwesomeIcon icon={faPlus}/> </button>
-                {/* <button type="button" className="btn-buy-product"><FontAwesomeIcon icon={faCartShopping}/>  </button> */}
-                <Link className="btn-buy-product" to={`/purchase/${product.id}`} onClick={handleAddOrder(product)}><FontAwesomeIcon icon={faCartShopping}/></Link>
-            </div>
-        </div>
+        <section>
+            <ul className="card-list-card">
+                <li><span className="title-list-products">{product.name_product}</span></li>
+                <li><span className="price-list-products">${product.price}</span></li>
+                <li><span className='description-list-products'>{product.description}</span></li>
+                <li><button type="button" className={`${productsList.find(pdt => pdt.id === product.id) ? "btn-delete-product" : "btn-cart"}`} onClick={() => handleAddOrRemoveProduct(product)}>
+                    {productsList.find(pdt => pdt.id === product.id) ? "Eliminar del " : "Agregar al "} carrito
+                </button></li>
+            </ul>
+        </section>
     );
 };
 
