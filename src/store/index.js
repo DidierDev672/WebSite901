8bb3ad87deaee98bbac409def7f95c9c9782c823
DidierDev672 <ariasdev672@gmail.com>
2022-12-13 02:59:04 +0100

    New order status

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import {  configureStore } from "@reduxjs/toolkit";
 import userReducer from "../reducers/users/userSlice";
 import cartReducer from "../reducers/cart/cartSlice";
 import blogReducer from "../reducers/blog/blogSlice";
+import ordersReducer from "../reducers/orders/ordersSlice";
 
 
 export default configureStore({
@@ -11,5 +12,6 @@ export default configureStore({
         user: userReducer,
         cart: cartReducer,
         blog: blogReducer,
+        orders: ordersReducer
     }
 });
