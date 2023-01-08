68994d9289780da42fe10702cfedcef47265c474
DidierDev672 <ariasdev672@gmail.com>
2023-01-07 23:42:04 -0500

    Enter of pure firestore storage services

---
+++ src/api/settings.ts
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAOBYHhp9lYtOR7KXIFH6TmvH6EV9KrwS0",
+    authDomain: "website-35275.firebaseapp.com",
+    projectId: "website-35275",
+    storageBucket: "website-35275.appspot.com",
+    messagingSenderId: "608887789880",
+    appId: "1:608887789880:web:81abcb87be6c104c0e49cd",
+    measurementId: "G-VY7MZ0NMQV"
+}
